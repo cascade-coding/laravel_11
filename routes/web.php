d0c4f40129214538e171740a895fdffc68b94a60
code-meta <fahim.x@yahoo.com>
2024-05-17 01:25:08 +0600

    add groups middleware

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 
-use App\Http\Middleware\UnderConstruction;
 
 
 Route::get('/', function () {
@@ -14,5 +13,15 @@ Route::get('/profile', function () {
     return view('profile');
 });
 
-Route::get('/dashboard', [DashboardController::class, 'show'])->middleware(UnderConstruction::class);
-// Route::get('/dashboard', [DashboardController::class, 'show'])->middleware([First::class, Second::class]);
+Route::get('/dashboard', [DashboardController::class, 'show'])->middleware('under-work');
+
+// add middleware to multiple routes with group
+// Route::middleware(['under-work'])->group(function () {
+//     Route::get('/', function () {
+//         return view('welcome');
+//     });
+
+//     Route::get('/profile', function () {
+//         return view('profile');
+//     });
+// });
