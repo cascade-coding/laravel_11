723408518425fabcf24bd0c67d836144c657f91e
code-meta <fahim.x@yahoo.com>
2024-05-16 06:13:05 +0600

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+Route::get('/dashboard', [DashboardController::class, 'show']);
