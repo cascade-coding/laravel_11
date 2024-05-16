80b179225f9887f09454961124b40ff7a3c4df5c
code-meta <fahim.x@yahoo.com>
2024-05-17 02:58:40 +0600

    middleware parameters

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -5,21 +5,19 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 
-class UnderConstruction
+class CheckRole
 {
     /**
      * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next, $role): Response
     {
-        // dd($request);
-        // dd($request->user);
-        // if($request->user === "admin") {}
-        dd($next($request));
+        if($role === 'guest'){
+            return redirect("/");
+        }
         return $next($request);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,24 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 
 
-
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/profile', function () {
     return view('profile');
-});
+})->middleware('role:guest');
 
 Route::get('/dashboard', [DashboardController::class, 'show'])->middleware('under-work');
-
-// add middleware to multiple routes with group
-// Route::middleware(['under-work'])->group(function () {
-//     Route::get('/', function () {
-//         return view('welcome');
-//     });
-
-//     Route::get('/profile', function () {
-//         return view('profile');
-//     });
-// });
