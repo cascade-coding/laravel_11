1f4e0373a9321e81d42d577140227c761e57f508
code-meta <fahim.x@yahoo.com>
2024-05-16 06:22:21 +0600

    assigning route middelware

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 
+use App\Http\Middleware\UnderConstruction;
+
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -11,4 +14,5 @@ Route::get('/profile', function () {
     return view('profile');
 });
 
-Route::get('/dashboard', [DashboardController::class, 'show']);
+Route::get('/dashboard', [DashboardController::class, 'show'])->middleware(UnderConstruction::class);
+// Route::get('/dashboard', [DashboardController::class, 'show'])->middleware([First::class, Second::class]);
